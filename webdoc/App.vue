3fcc402362b114e0d4cbb8cd449141230dbbafb4
qiyi <qiyi@kaishiba.com>
2019-04-25 15:15:10 +0800

    update docs

--- webdoc/App.vue
+++ webdoc/App.vue
@@ -84,8 +84,26 @@ export default {
         this.showSideBar = false
       };
     })
+    //友盟统计
+    const script = document.createElement('script');
+    script.src = 'https://s65.cnzz.com/z_stat.php?id=1277362177&web_id=1277362177';
+    script.language = 'JavaScript';
+    document.body.appendChild(script);
+  },
+  watch: {
+    '$route' () {
+      if (window._czc) {
+        let location = window.location;//路由变化
+        let contentUrl = location.hash;//自定义当前url，可带上路由以此区分每个页面
+        console.log(contentUrl);
+        let refererUrl = '/';
+        window._czc.push(["_setAutoPageview", false]);
+        window._czc.push(["_trackPageview", contentUrl, refererUrl])
+      }
+    }
   }
 }
+
 </script>
 
 <style>
